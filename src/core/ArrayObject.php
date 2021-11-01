1cd7a16c7b108607d78aa97b5c0755814bf12ac3
Bruce Dou <doubaokun@gmail.com>
2021-11-01 16:21:35 +0000

    PHP8.1 fixes

--- src/core/ArrayObject.php
+++ src/core/ArrayObject.php
@@ -68,7 +68,7 @@ class ArrayObject implements ArrayAccess, Serializable, Countable, Iterator
         return current($this->array);
     }
 
-    public function key(): mixed
+    public function key(): \mixed
     {
         return key($this->array);
     }
