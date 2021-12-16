92f1e4bc67bc1f005706989f88dada2a6d4135d1
Bruce Dou <doubaokun@gmail.com>
2021-12-16 18:41:04 +0000

    Updated openmetrics output

--- src/core/Server/Helper.php
+++ src/core/Server/Helper.php
@@ -181,24 +181,70 @@ class Helper
 
     public static function statsToOpenMetrics(array $stats)
     {
+        $event_workers = "";
+        foreach ($stats['event_workers'] as $stat) {
+            $event_workers .= . "openswoole_event_workers_start_time{${stat['worker_id']}} ${stat['start_time']}\n";
+            $event_workers .= . "openswoole_event_workers_start_time_seconds{${stat['worker_id']}} ${stat['start_time_seconds']}\n";
+            $event_workers .= . "openswoole_event_workers_request_count{${stat['worker_id']}} ${stat['request_count']}\n";
+            $event_workers .= . "openswoole_event_workers_dispatch_count{${stat['worker_id']}} ${stat['dispatch_count']}\n";
+        }
+
+        $task_workers = "";
+        foreach ($stats['task_workers'] as $stat) {
+            $task_workers .= . "openswoole_task_workers_start_time{${stat['worker_id']}} ${stat['start_time']}\n";
+            $task_workers .= . "openswoole_task_workers_start_time_seconds{${stat['worker_id']}} ${stat['start_time_seconds']}\n";
+        }
+
+        $user_workers = "";
+        foreach ($stats['user_workers'] as $stat) {
+            $user_workers .= . "openswoole_user_workers_start_time{${stat['worker_id']}} ${stat['start_time']}\n";
+            $user_workers .= . "openswoole_user_workers_start_time_seconds{${stat['worker_id']}} ${stat['start_time_seconds']}\n";
+        }
+
         return "# HELP openswoole_up Is OpenSwoole server up?\n"
-                . "# TYPE openswoole_up gauge\n"
+                . "# TYPE openswoole_up stateset\n"
                 . "openswoole_up ${stats['up']}\n"
-                // . "openswoole_version ${stats['version']}\n"
+                . "# TYPE openswoole_version info\n"
+                . "openswoole_version ${stats['version']}\n"
+                . "# TYPE openswoole_start_time_seconds counter\n"
+                . "openswoole_reactor_num ${stats['reactor_num']}\n"
+                . "# TYPE openswoole_reactor_num counter\n"
+                . "openswoole_requests_total ${stats['requests_total']}\n"
+                . "# TYPE openswoole_requests_total counter\n"
+                . "openswoole_start_time ${stats['start_time']}\n"
+                . "# TYPE openswoole_start_time counter\n"
+                . "openswoole_max_conn ${stats['max_conn']}\n"
+                . "# TYPE openswoole_max_conn counter\n"
+                . "openswoole_coroutine_num ${stats['coroutine_num']}\n"
+                . "# TYPE openswoole_coroutine_num counter\n"
                 . "openswoole_start_time_seconds ${stats['start_time_seconds']}\n"
+                . "# TYPE openswoole_workers_total counter\n"
                 . "openswoole_workers_total ${stats['workers_total']}\n"
+                . "# TYPE openswoole_workers_idle counter\n"
                 . "openswoole_workers_idle ${stats['workers_idle']}\n"
+                . "# TYPE openswoole_task_workers_total counter\n"
                 . "openswoole_task_workers_total ${stats['task_workers_total']}\n"
+                . "# TYPE openswoole_task_workers_idle counter\n"
                 . "openswoole_task_workers_idle ${stats['task_workers_idle']}\n"
+                . "# TYPE openswoole_user_workers_total counter\n"
                 . "openswoole_user_workers_total ${stats['user_workers_total']}\n"
+                . "# TYPE openswoole_dispatch_total counter\n"
                 . "openswoole_dispatch_total ${stats['dispatch_total']}\n"
+                . "# TYPE openswoole_connections_accepted counter\n"
                 . "openswoole_connections_accepted ${stats['connections_accepted']}\n"
+                . "# TYPE openswoole_connections_active counter\n"
                 . "openswoole_connections_active ${stats['connections_active']}\n"
+                . "# TYPE openswoole_connections_closed counter\n"
                 . "openswoole_connections_closed ${stats['connections_closed']}\n"
+                . "# TYPE openswoole_reload_count counter\n"
                 . "openswoole_reload_count ${stats['reload_count']}\n"
+                . "# TYPE openswoole_reload_last_time counter\n"
                 . "openswoole_reload_last_time ${stats['reload_last_time']}\n"
+                . "# TYPE openswoole_worker_vm_object_num counter\n"
                 . "openswoole_worker_vm_object_num ${stats['worker_vm_object_num']}\n"
+                . "# TYPE openswoole_worker_vm_resource_num counter\n"
                 . "openswoole_worker_vm_resource_num ${stats['worker_vm_resource_num']}\n"
-                . "openswoole_worker_memory_usage ${stats['worker_memory_usage']}\n";
+                . "# TYPE openswoole_worker_memory_usage counter\n"
+                . "openswoole_worker_memory_usage ${stats['worker_memory_usage']}\n{$event_workers}{$task_workers}{$user_workers}";
     }
 }
